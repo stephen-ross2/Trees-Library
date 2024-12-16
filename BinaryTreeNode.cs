77d6b03df3888c6e4f7fb32fae5849cbb2a6978a
stephen-ross2 <stephen.ross2@outlook.com>
2024-12-16 14:58:10 -0600

    Add project files.

---
+++ BinaryTreeNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees_Library
+{
+    public class BinaryTreeNode<T>
+        : TreeNode<T>
+    {
+        public new BinaryTreeNode<T>?[] Children { get; set; }
+        = [null, null];
+
+        public BinaryTreeNode<T>? Left
+        {
+            get { return Children[0]; }
+            set { Children[0] = value; }
+        }
+
+        public BinaryTreeNode<T>? Right
+        {
+            get { return Children[1]; }
+            set { Children[1] = value; }
+        }
+    }
+
+        public class BinaryTree<T>
+        {
+            public BinaryTreeNode<T>? Root { get; set; }
+            public int Count { get; set; }
+        }
+    }
+
+
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+
+namespace Trees_Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new()
+            { Root = new() { Data = 100 } };
+
+            tree.Root.Children =
+            [
+            new() { Data = 50, Parent = tree.Root},
+            new() { Data = 1, Parent = tree.Root },
+            new() { Data = 150, Parent = tree.Root },
+            ];
+
+            tree.Root.Children[2].Children =
+            [
+            new() {Data = 30, Parent = tree.Root.Children[2] },
+            new() {Data = 5, Parent = tree.Root.Children[2] },
+            new() {Data = 11, Parent = tree.Root.Children[2] },
+            ];
+
+            tree.Root.Children[2].Children[0].Children =
+            [
+            new() {Data = 96, Parent = tree.Root.Children[2].Children[0] },
+            new() {Data = 9, Parent = tree.Root.Children[2].Children[0] },
+            ];
+
+            tree.Root.Children[1].Children =
+            [
+            new() {Data = 70, Parent = tree.Root.Children[1] },
+            new() {Data = 61, Parent = tree.Root.Children[1] },
+            ];
+
+            tree.Root.Children[0].Children =
+            [
+            new() {Data = 12, Parent = tree.Root.Children[0].Children[0] },
+            ];
+
+            tree.Root.Children[0].Children[0].Children =
+            [
+            new() {Data = 45, Parent = tree.Root.Children[0].Children[0] },
+            new() {Data = 21, Parent = tree.Root.Children[0].Children[0] },
+            ];
+
+            tree.Root.Children[0].Children[0].Children[1].Children =
+            [
+            new() {Data = 6, Parent = tree.Root.Children[0].Children[0].Children[1] },
+            ];
+
+
+        }
+    }
+}
